b470c69da5a895b325cabb8a776a639abfed3454
Vance <thevancetan@gmail.com>
2020-08-02 17:42:35 +0800

    Added more basic routes for Donor and Artefact and basic Auth base for API

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
+const genJwt = require("jsonwebtoken");
+const jwt = require("express-jwt");
+const jwtDecode = require("jwt-decode");
 const { PrismaClient } = require("@prisma/client");
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +16,28 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 const prisma = new PrismaClient();
+
+const createToken = (admin) => {
+  return genJwt.sign(
+    {
+      sub: admin._id,
+      email: admin.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      algorithm: "HS256",
+      audience: "api.thenextstage",
+      issuer: "api.thenextstage",
+    }
+  );
+};
+
+const requireAuth = jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+  audience: "api.thenextstage",
+  issuer: "api.thenextstage",
+});
 
 app.post("/api/donor", async (req, res) => {
   try {
@@ -29,20 +56,19 @@ app.post("/api/donor", async (req, res) => {
       },
     });
 
-    if (!donor) {
-      return res.status(404).json({
-        message: "Something went wrong.",
-      });
-    } else {
-      return res.status(200).json({ message: "Success!", donor });
-    }
+    return res.status(201).json({ data: donor });
   } catch (err) {
     console.log(err);
-    return res.status(404).json({ message: "Something went wrong." });
+    return res.status(500).json({
+      error: {
+        code: "500",
+        message: "Something went wrong.",
+      },
+    });
   }
 });
 
-app.get("/api/magic=:key", async (req, res) => {
+app.get("/api/magic/:key", async (req, res) => {
   try {
     const key = req.params.key;
 
@@ -57,32 +83,90 @@ app.get("/api/magic=:key", async (req, res) => {
 
     if (!magic) {
       return res.status(404).json({
-        message: "Something went wrong.",
+        error: {
+          code: "404",
+          message: "Not found.",
+        },
       });
     } else {
       const donor = magic.donor;
-      return res.status(200).json({ message: "Success!", donor });
+      return res.status(200).json({ data: donor });
     }
   } catch (err) {
     console.log(err);
-    return res.status(404).json({ message: "Something went wrong." });
+    return res.status(500).json({
+      error: {
+        code: "500",
+        message: "Something went wrong.",
+      },
+    });
   }
 });
 
-app.get("/api/artefacts/count", async (req, res) => {
+app.delete("/api/donor/:id", async (req, res) => {
   try {
-    const count = await prisma.artefact.count();
+    const id = req.params.id;
 
-    if (isNaN(count)) {
-      return res.status(404).json({
-        message: "Something went wrong.",
-      });
-    } else {
-      return res.status(200).json({ message: "Success!", count });
-    }
+    await prisma.donor.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(204).json({});
   } catch (err) {
     console.log(err);
-    return res.status(404).json({ message: "Something went wrong." });
+    return res.status(404).json({
+      error: {
+        code: "404",
+        message: "Invalid ID.",
+      },
+    });
+  }
+});
+
+app.post("/api/artefact", async (req, res) => {
+  try {
+    const { something, somethingTwo } = req.body;
+
+    const artefact = await prisma.artefact.create({
+      data: {
+        email,
+        nickname,
+        magic: {
+          create: {},
+        },
+      },
+      include: {
+        magic: true,
+      },
+    });
+
+    return res.status(201).json({ data: artefact });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: {
+        code: "500",
+        message: "Something went wrong.",
+      },
+    });
+  }
+});
+
+app.get("/api/artefact", async (req, res) => {
+  try {
+    const artefacts = await prisma.artefact.findMany({ skip: 6, take: 6 });
+
+    return res.status(200).json({ data: artefacts });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: {
+        code: "500",
+        message: "Something went wrong.",
+      },
+    });
   }
 });
 
@@ -98,14 +182,60 @@ app.get("/api/artefact/:id", async (req, res) => {
 
     if (!artefact) {
       return res.status(404).json({
-        message: "Something went wrong.",
+        error: {
+          code: "404",
+          message: "Artefact not found.",
+        },
       });
     } else {
-      return res.status(200).json({ message: "Success!", artefact });
+      return res.status(200).json({ data: artefact });
     }
   } catch (err) {
     console.log(err);
-    return res.status(404).json({ message: "Something went wrong." });
+    return res.status(500).json({
+      error: {
+        code: "500",
+        message: "Something went wrong.",
+      },
+    });
+  }
+});
+
+app.delete("/api/artefact/:id", requireAuth, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await prisma.artefact.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(204).json({});
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({
+      error: {
+        code: "404",
+        message: "Invalid ID.",
+      },
+    });
+  }
+});
+
+app.get("/api/artefacts/count", async (req, res) => {
+  try {
+    const count = await prisma.artefact.count();
+
+    return res.status(200).json({ data: count });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: {
+        code: "500",
+        message: "Something went wrong.",
+      },
+    });
   }
 });
 
